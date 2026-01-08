d7b8c4caf0cdf609fa79abd8b632cec4aaa33f0d
Ahmet Oeztuerk <Ahmet.Oeztuerk@consol.de>
2026-01-08 17:06:59 +0100

    revise performance data collection
    should now be thread safe, as each caller manages its own collection and only uses member functions

--- PerformanceData.go
+++ PerformanceData.go
@@ -7,68 +7,123 @@ import (
 	"sync"
 )
 
-type PerformanceData map[string]interface{}
+// PerformanceData is a map with string keys and any values, essentially a dictionary
+// Since its an dictionary, always add add a label
+// "label" = "health_rate"
+// "value" = 80
+// "critical" = 160
+type PerformanceData map[string]any
 
-var (
-	p      []PerformanceData = []PerformanceData{}
-	pMutex                   = &sync.Mutex{}
-)
-
-// NewPerformanceData adds a PerformanceData object which can be expanded with further information
-func NewPerformanceData(label string, value float64) *PerformanceData {
-	return NewPerformanceDataString(label, strconv.FormatFloat(value, 'f', -1, 64))
+// This is a struct that users of the library should use.
+// They have to manage this object through functions in this file
+// Library clients should get an instance of this and keep calling its methods
+type PerformanceDataCollection struct {
+	data      []PerformanceData
+	dataMutex *sync.Mutex
 }
 
-// NewPerformanceDataString adds a PerformanceData object which can be expanded with further information
-func NewPerformanceDataString(label, value string) *PerformanceData {
-	pMutex.Lock()
-	p = append(p, PerformanceData{"label": label, "value": value})
-	newOne := &(p[len(p)-1])
-	pMutex.Unlock()
-	return newOne
+// Returns an empty PerformanceDataCollection
+func NewPerformanceDataCollection() PerformanceDataCollection {
+	return PerformanceDataCollection{
+		data:      make([]PerformanceData, 0),
+		dataMutex: &sync.Mutex{},
+	}
 }
 
-// Unit adds an unit string to the PerformanceData
-func (p PerformanceData) Unit(unit string) PerformanceData {
-	p["unit"] = unit
-	return p
+// Adds a new PerformanceData element to the data array
+// Use the label to get the reference to the PerformanceData later on
+func (collection *PerformanceDataCollection) AddPerformanceData(label string, value string) {
+	collection.dataMutex.Lock()
+	collection.data = append(collection.data, PerformanceData{"label": label, "value": value})
+	collection.dataMutex.Unlock()
 }
 
-// Warn adds the threshold to the PerformanceData
-func (p PerformanceData) Warn(warn *Threshold) PerformanceData {
-	p["warn"] = warn
-	return p
+// Calls collection.AddPerformanceData after converting float to string
+func (collection *PerformanceDataCollection) AddPerformanceDataFloat64(label string, value float64) {
+	collection.AddPerformanceData(label, strconv.FormatFloat(value, 'f', -1, 64))
 }
 
-// Crit adds the threshold to the PerformanceData
-func (p PerformanceData) Crit(crit *Threshold) PerformanceData {
-	p["crit"] = crit
-	return p
+// Internal function to find a PerformanceData with specified label
+func (collection *PerformanceDataCollection) findPerformanceData(label string) (*PerformanceData, error) {
+	for index, pd := range collection.data {
+		if pd_label, ok := pd["label"]; ok && pd_label == label {
+			return &collection.data[index], nil
+		}
+	}
+	return nil, fmt.Errorf("no performance data with the label '%s' found", label)
 }
 
-// Min adds the float64 to the PerformanceData
-func (p PerformanceData) Min(min float64) PerformanceData {
-	p["min"] = min
-	return p
+// Adds a field called "unit" to the PerformanceData with the label
+func (collection *PerformanceDataCollection) Unit(label string, unit string) error {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	pd, err := collection.findPerformanceData(label)
+	if err != nil {
+		return err
+	}
+	(*pd)["unit"] = unit
+	return nil
 }
 
-// Min adds the float64 to the PerformanceData
-func (p PerformanceData) Max(max float64) PerformanceData {
-	p["max"] = max
-	return p
+// Adds a field called "unit" to the PerformanceData with the label
+func (collection *PerformanceDataCollection) Warn(label string, warn *Threshold) error {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	pd, err := collection.findPerformanceData(label)
+	if err != nil {
+		return err
+	}
+	(*pd)["warn"] = warn
+	return nil
 }
 
-// toString prints this PerformanceData
-func (p PerformanceData) toString() string {
+// Adds a field called "unit" to the PerformanceData with the label
+func (collection *PerformanceDataCollection) Crit(label string, crit *Threshold) error {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	pd, err := collection.findPerformanceData(label)
+	if err != nil {
+		return err
+	}
+	(*pd)["crit"] = crit
+	return nil
+}
+
+// Adds a field called "unit" to the PerformanceData with the label
+func (collection *PerformanceDataCollection) Min(label string, min float64) error {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	pd, err := collection.findPerformanceData(label)
+	if err != nil {
+		return err
+	}
+	(*pd)["min"] = min
+	return nil
+}
+
+// Adds a field called "unit" to the PerformanceData with the label
+func (collection *PerformanceDataCollection) Max(label string, max float64) error {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	pd, err := collection.findPerformanceData(label)
+	if err != nil {
+		return err
+	}
+	(*pd)["max"] = max
+	return nil
+}
+
+// internal function to print a PerformanceData
+func (pd PerformanceData) toString() string {
 	var toPrint bytes.Buffer
 
-	toPrint.WriteString(fmt.Sprintf("'%s'=%s", p["label"], p["value"]))
-	if unit, ok := p["unit"]; ok {
+	toPrint.WriteString(fmt.Sprintf("'%s'=%s", pd["label"], pd["value"]))
+	if unit, ok := pd["unit"]; ok {
 		toPrint.WriteString(unit.(string))
 	}
 	toPrint.WriteString(";")
 	addThreshold := func(key string) {
-		if value, ok := p[key]; ok && value != nil {
+		if value, ok := pd[key]; ok && value != nil {
 			if t := value.(*Threshold); t != nil {
 				toPrint.WriteString(t.input)
 			}
@@ -79,7 +134,7 @@ func (p PerformanceData) toString() string {
 	addThreshold("crit")
 
 	addFloat := func(key string) {
-		if value, ok := p[key]; ok {
+		if value, ok := pd[key]; ok {
 			toPrint.WriteString(strconv.FormatFloat(value.(float64), 'f', -1, 64))
 		}
 	}
@@ -90,14 +145,33 @@ func (p PerformanceData) toString() string {
 	return toPrint.String()
 }
 
-// PrintPerformanceData prints all PerformanceData
-func PrintPerformanceData() string {
+// Finds and prints the PerformanceData found in this collection
+func (collection *PerformanceDataCollection) PrintPerformanceData(label string) (string, error) {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	pd, err := collection.findPerformanceData(label)
+	if err != nil {
+		return "", err
+	}
+
+	return (*pd).toString(), nil
+}
+
+// Prints all PerformanceData to a string in this collection
+func (collection *PerformanceDataCollection) PrintAllPerformanceData() string {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
 	var toPrint bytes.Buffer
-	pMutex.Lock()
-	for _, perfData := range p {
+	for _, perfData := range collection.data {
 		toPrint.WriteString(perfData.toString())
 		toPrint.WriteString(" ")
 	}
-	pMutex.Unlock()
 	return toPrint.String()
+}
+
+// Clears all PerformanceData stored in this collection
+func (collection *PerformanceDataCollection) ClearPerformanceCollection() {
+	collection.dataMutex.Lock()
+	defer collection.dataMutex.Unlock()
+	collection.data = make([]PerformanceData, 0)
 }
